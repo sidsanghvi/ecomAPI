dc4ba3d919a7a12e5a22169207a3f49be877e9ff
Sid Sanghvi <sidsanghvi7@gmail.com>
2020-08-26 12:29:12 +0530

    legacy rest api done

--- products/views.py
+++ products/views.py
@@ -33,14 +33,3 @@ def product_detail(reauest, pk):
             }},
             status=404)
     return response
-#from django.views.generic.detail import DetailView
-#from django.views.generic.list import ListView
-
-# class ProductDetailView(DetailView):
-#     model = Product
-#     template_name = 'products/product_detail.html'
-
-
-# class ProductListView(ListView):
-#     model = Product
-#     template_name = 'products/product_list.html'
